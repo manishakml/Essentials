539a816f4a25581a00f271231b6e94c7065d847f
Manisha Kamal <manishakml@gmail.com>
2016-11-15 15:26:43 -0800

    Update closest-integers-same-weight.cpp

--- closest-integers-same-weight.cpp
+++ closest-integers-same-weight.cpp
@@ -1,1 +1,11 @@
 //EPI 5.4
+
+unsigned long closest_integer_same_weight(unsigned long &n) {
+  for(int i = 0; i < 63; i++) {
+    if( ((n>>i) & 1) ^ ((n >> (i+1)) & 1) ) {
+      n ^= (1UL << i) | (1UL << (i+1));
+      return n;
+    }
+  }
+  throw invalid_argument("all bits are 0 or 1");
+}
