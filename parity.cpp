c5fa8a5df3d6404381972fec62b5b3895c94b090
Manisha Kamal <manishakml@gmail.com>
2016-11-15 14:53:54 -0800

    Update parity.cpp

--- parity.cpp
+++ parity.cpp
@@ -1,1 +1,19 @@
+// EPI 5.1
+short parity_compute (unsigned long n) {
+  short res = 0;
+  while (n) {
+    n = (n & (n-1));
+    res ^= 1;
+  }
+  return res;
+}
 
+unsigned long 16-BIT  = pow(2,16)
+short pc[16-BIT];
+for(unsigned long i = 0; i < 16-BIT; i++) {
+  pc[i] = parity_compute(i);
+}
+
+short parity_compute2 (unsigned long n) {
+  return pc[n >> 48] ^ pc[(n >> 32) & 0xffff] ^ pc[(n >> 16) & 0xffff] ^ pc[n & 0xffff];
+}
