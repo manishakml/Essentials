c8cbff36d21ce9f3cbafb2b5e7a378d1ce92a195
Manisha Kamal <manishakml@gmail.com>
2016-11-15 15:40:03 -0800

    Update power-set.cpp

--- power-set.cpp
+++ power-set.cpp
@@ -1,1 +1,17 @@
 //EPI 5.5
+void power_set(const vector<int> &S) {
+  int n = S.size();
+  int subnum = pow(2,n);
+  vector<vector<int>> res;
+  for (int i = 0; i < subnum; i++) {
+    vector<int> ans;
+    for(int j = 0; j < n; j++) {
+      if(i & (1<<j)) {
+        ans.push_back(S[j]); // OR cout << S[j] << ",";
+      }
+    }
+    res.push_back(ans);    //OR cout << '\n';
+    ans.clear();
+  }
+  return res;
+}
