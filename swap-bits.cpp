22369171b8ee64685d83d22366c57196717432f5
Manisha Kamal <manishakml@gmail.com>
2016-11-15 15:03:23 -0800

    Update swap-bits.cpp

--- swap-bits.cpp
+++ swap-bits.cpp
@@ -1,1 +1,10 @@
-// EPI 5.2
+// EPI 5.2 
+long swap_bits(long n, const int &i, const int &j) {
+  int a = (n >> i) & 1;
+  int b = (n >> j) & 1;
+  
+  if (a ^ b) {
+    n ^= (1L << i) | (1L << j);
+  }
+  return n;
+}
