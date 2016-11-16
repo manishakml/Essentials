fd30c270f64b06f05adef70f0ae9f768b5e9f084
Manisha Kamal <manishakml@gmail.com>
2016-11-15 22:31:30 -0800

    Update division-use-addSubMul.cpp

--- division-use-addSubMul.cpp
+++ division-use-addSubMul.cpp
@@ -1,1 +1,20 @@
 //EPI 5.14
+unsigned divide(const unsigned &dividend, const unsigned &divisor) {
+  if(dividend < divisor) return 0;
+  unsigned quotient;
+  unsigned curDivisor = divisor;
+  unsigned quotientBase = 1;
+  
+  while(dividend >= divisor) {
+    if(dividend >= curDivisor) {
+      dividend -= curDivisor;
+      quotient += quotientBase;
+      qutientBase *= 2;
+      curDivisor *= 2;
+    } else {
+      quotientBase  >> 1 ;
+      curDivisor >> 1;
+    }
+  }
+  return quotient;
+}
