a35c72f133b0d7a45e9baa1120e9db8f6d02ea3e
Manisha Kamal <manishakml@gmail.com>
2016-11-15 22:07:02 -0800

    Update intersecting_rectangles.cpp

--- intersecting_rectangles.cpp
+++ intersecting_rectangles.cpp
@@ -1,1 +1,17 @@
 //EPI 5.12
+typedef struct rectangle{
+  int x;
+  int y;
+  int w;
+  int h;
+}Rectangle;
+
+bool intersecting(const Rectangle &R, const Rectangle &S) {
+  return R.x+R.w >= S.x &&  R.x <= S.x+S.w && R.y <= S.y+S.h && R.y+R.h >= S.y;
+}
+Rectangle intersecting_rectangle(Rectangle R, Rectangle S) {
+  if(intersecting(R,S)) {
+    return {max(R.x, S.x), max(R.y,S.y), min(R.x+R.w,S.x+S.w)-max(R.x,S.x), min(R.y+R.h, S.y+S.h)-max(R.y,S.y)};
+  } else {
+    return {0,0,-1,-1};
+}
