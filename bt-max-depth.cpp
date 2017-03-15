8fbecb16d88113818e412bbe1175d5148a56341b
Manisha Kamal <manishakml@gmail.com>
2017-03-15 14:42:24 -0700

    Update bt-max-depth.cpp

--- bt-max-depth.cpp
+++ bt-max-depth.cpp
@@ -1,1 +1,16 @@
+/* LC 104
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
 
+#include<iostream>
+using namespace std;
+
+int depth(Node *root) {
+  return root? 1+ max(depth(root->left), depth(root->right)) : 0;
+}
+
+/* Tested.
+ * Time complexity: O(n) where n is the number of nodes.
+ * Space complexity: O(n).
+ */
