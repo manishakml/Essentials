74bdeb57ab8e1b337a4c985e9a4429648869cba1
Manisha Kamal <manishakml@gmail.com>
2017-03-30 13:57:40 -0700

    Update excel-sheet-int2str.cpp

--- excel-sheet-int2str.cpp
+++ excel-sheet-int2str.cpp
@@ -1,1 +1,32 @@
+/* LC 168
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ * For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+  */
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+string excel(int n) {
+  string res="";
+  while(n) {
+    res += 'A' + (n-1)%26;
+    n = (n-1)/26;
+  }
+  reverse(res.begin(),res.end());
+  return res;
+}
+
+/* Tested.
+ * Note: (n-1) has been used to handle cases such as 'Z' which return 0 on modulus
+ * Time complexity: O(log n) with base 26.
+ * Space complexity: O(1)
+ */
+  
