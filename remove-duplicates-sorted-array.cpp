7e881efaddcf0b868c61e3fd75b70eb4c2cfc6f6
Manisha Kamal <manishakml@gmail.com>
2017-03-31 15:57:43 -0700

    Update remove-duplicates-sorted-array.cpp

--- remove-duplicates-sorted-array.cpp
+++ remove-duplicates-sorted-array.cpp
@@ -1,1 +1,28 @@
+/* LC 26
+ * 
+ */
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int removeDuplicates(vector<int>& nums) {
+        int w = 0, r = 1, n = nums.size();
+        if(n < 2) {
+            return n;
+        }
+        while(r < n) {
+            if (r < n && nums[r] == nums[w]) {
+                r++;
+                continue;
+            } else {
+                nums[++w] = nums[r++];
+            }
+        }
+        return w+1;
+}
+
+/* Tested
+ * Note: Work out all corner cases. Good egs: [1,1,2], [1,1] etc.
+ * Time complexity: O(n) where n is the number of elements.
+ * Space complexity: O(1)
+ */
