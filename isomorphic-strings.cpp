db00a13720098e2e5446cd6d0535afdded3775ff
unyieldingGlacier <manishakml@gmail.com>
2017-04-20 16:11:20 -0700

    Update isomorphic-strings.cpp

--- isomorphic-strings.cpp
+++ isomorphic-strings.cpp
@@ -23,8 +23,66 @@
   }
   return true;
  }
+      
  
 /* Tested.
  * Time complexity: O(n) where n is the length of the string.
  * Space complexity: O(1).
  */
+
+//Variations
+//3 strings instead of 2. return true if all are isomorphic. No change in complexity
+bool iso3(string s, string t, string u) {
+    return isIsomorphic(s,t) && isIsomorphic(t,u);
+}
+//3 strings instead of 2. return true if any 2 are isomorphic. No change in complexity
+bool iso3_1(string s, string t, string u) {
+    return isIsomorphic(s,t) || isIsomorphic(t,u) || isIsomorphic(s,u);
+}
+// n strings. return true if all are isomorphic. Time complexity: O(n*m) = n strings of length m.
+bool isoN(vector<string>& A) {
+    bool res = true;
+    for(int i = 0; i < A.size()-1; i++) {
+        res &= isIsomorphic(A[i],A[i+1]);
+    }
+    return res;
+}
+  // n strings. return true if any pair is isomorphic . Time complexity: O(n^2m)       
+bool isoN_1(vector<string>& A) {
+        for(int i = 0; i < A.size()-1; i++) {
+            for(int j = i+1; j < A.size(); j++) {
+                 if(isIsomorphic(A[i],A[j]))
+                     return true;
+            }
+        }
+        return false;
+}
+// n strings. group isomorphic strings together. Time complexity: O(n^2m). Space complexity: O(nm) if we count in terms of individual chars.
+vector<vector<string> > iso_group(vector<string> &A) {
+    unordered_map<string,vector<string> > m;
+    unordered_set<string> s;
+    vector<vector<string> > res;
+    for(int i = 0; i < A.size(); i++) {
+        s.insert(A[i]);
+    }
+    for(int i = 0; i < A.size()-1; i++) {
+        if(s.find(A[i]) == s.end()) continue;
+        m[A[i]].push_back(A[i]);
+        s.erase(A[i]);
+        for(int j = i+1; j < A.size(); j++) {
+            if(s.find(A[j]) == s.end()) continue;
+            if(isIsomorphic(A[i],A[j])) {
+                    m[A[i]].push_back(A[j]);
+                    s.erase(A[j]);
+            }
+        }
+    }
+    if(!s.empty()) {
+        m[A[A.size()-1]].push_back(A[A.size()-1]);
+        s.erase(A[A.size()-1]);
+    }
+    for (unordered_map<string,vector<string> >::iterator it=m.begin(); it!=m.end(); ++it){
+       res.push_back(it->second);
+    }
+    return res;
+}
