c98af1094386943743e0c6e36da16b377d4b6b14
unyieldingGlacier <manishakml@gmail.com>
2017-04-25 16:56:40 -0700

    Update integer-to-roman.cpp

--- integer-to-roman.cpp
+++ integer-to-roman.cpp
@@ -1,1 +1,20 @@
-/* LC 
+/* LC 12
+ * Given an integer, convert it to a roman numeral.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<string> M = {"","M","MM","MMM"}
+vector<string> C = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"}
+vector<string> X = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"}
+vector<string> I = {"","I","II","III","IV","V","VI","VII","VIII","IX"}
+string toRoman(int n) {
+  return M[n/1000] + C[(n%1000)/100] + X[(n%100)/10] + I[n%10];
+}
+
+/* Tested.
+ * Time complexity: O(1)
+ * Space complexity: O(1)
+ */
