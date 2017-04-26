bf173329a3a75ddbd7833b051e70a04581660687
unyieldingGlacier <manishakml@gmail.com>
2017-04-25 17:17:53 -0700

    Update integer-to-roman.cpp

--- integer-to-roman.cpp
+++ integer-to-roman.cpp
@@ -11,6 +11,7 @@ vector<string> C = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
 vector<string> X = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
 vector<string> I = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
 string toRoman(int n) {
+  if(n < 0) return ""; //handle negative
   return M[n/1000] + C[(n%1000)/100] + X[(n%100)/10] + I[n%10];
 }
 
