bf36cfe8ab2b6300eb437c6f24007aa086200f4b
unyieldingGlacier <manishakml@gmail.com>
2017-05-01 15:56:28 -0700

    Update paint-house2.cpp

--- paint-house2.cpp
+++ paint-house2.cpp
@@ -89,6 +89,46 @@ void find_min2(vector<vector<int>> &costs, int& f, int& s, int& k, int& i){
         return ans;
     }
 
+// If original array cannot be modified, then use explicit O(k) space. We need sval and fval since dp is getting modified. Also, for cases where s remains -1 (n: 1), we need the conditional assignment for fval and sval.
+void find_min2(int dp[], int& f, int& s, int& k){
+        for(int j = 0; j < k; j++) {
+            if(f == -1 || dp[j] < dp[f]) {
+                s=f;
+                f=j;
+            } else if(s==-1 || dp[j] < dp[s]) {
+                s=j;
+            }
+        }
+    }
+    
+    int minCostII(vector<vector<int>>& costs) {
+        if (costs.size() == 0 || costs[0].size() == 0) return 0;
+  
+        int n = costs.size(), k = costs[0].size();
+  
+        if(k == 1 && n > 1) return INT_MAX;
+  
+        int f, s, ans = INT_MAX, fval,sval;
+        
+        int dp[k];
+        for(int j = 0; j < k; j++) {
+            dp[j] = costs[0][j];
+        }
+    
+        for(int i = 0; i < n; i++) {
+            if(i > 0) {
+                for(int j = 0; j < k; j++) {
+                    dp[j] = costs[i][j] + ((j == f)? sval:fval);
+                }
+            }
+            f = -1, s = -1;
+            find_min2(dp,f,s,k);
+            if(f > -1) fval = dp[f];
+            if(s > -1) sval = dp[s];
+        }
+        
+        return dp[f];
+    }
 /* Tested.
  * Note: O(nk^2) approach is direct modification of paint-house1 where extra k loop scans the minimum of prev level.
  * Time complexity: O(nk) where n is the number of houses, k is the number of colors.
