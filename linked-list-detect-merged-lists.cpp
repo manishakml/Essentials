8f62d7b03fbb8369360bc07a48a35abc15207bad
unyieldingGlacier <manishakml@gmail.com>
2017-05-10 14:12:21 -0700

    Update linked-list-detect-merged-lists.cpp

--- linked-list-detect-merged-lists.cpp
+++ linked-list-detect-merged-lists.cpp
@@ -5,17 +5,32 @@
 #include<iostream>
 using namespace std;
 
-bool cycle(List *head) {
-        List* f = head, *s = head;
+List *detectCycle(List *head) {
+        List* f = head;
+        List* s = head;
         
-        while(s && s->next && s->next->next){
+        while(s && s->next && s->next->next) {
             f = f->next;
             s = s->next->next;
             if(f == s) {
-                return true;
+                int c = 0;
+                do{
+                    s = s->next;
+                    c++;
+                }while(f!=s);
+                f = head; s = head;
+                while(c) {
+                    s = s->next;
+                    c--;
+                }
+                while(f!=s) {
+                    f = f->next;
+                    s = s->next;
+                }
+                return f;
             }
         }
-        return false;
+        return NULL;
 }
 
 bool detect_merge(List* h1, List* h2) {
@@ -30,17 +45,20 @@ bool detect_merge(List* h1, List* h2) {
       h2 = h2->next;
     }
     if(h1 == h2) {
+      //if we have to return start of the merge: Use linked-list-intersection.cpp logic here.
       return true;
     }
-  // Both have cycles: They could be standalone lsits with cycles OR they could be merged at some point and share the cycle from there. So, head_of_cycle_1 should be able to travel the loop to find head_of_cycle_2.
+  // Both have cycles: They could be standalone lists with cycles OR they could be merged at some point and share the cycle from there. So, head_of_cycle_1 should be able to travel the loop to find head_of_cycle_2.
   } else if(a && b) {
     if(a == b) {
+      //If we have to return point of merge, return a (or b) here.
       return true;
     }
     List *tmp = a;
     do{
       tmp = tmp->next;
       if(tmp == b) {
+        // If we have to return point of merge, return tmp here.
         return true;
       }
     }while(tmp != a);
