4d6b6baa7c80bb84a7e70fc5345b96cabe523149
unyieldingGlacier <manishakml@gmail.com>
2017-05-22 16:22:11 -0700

    Update bst-kth-smallest.cpp

--- bst-kth-smallest.cpp
+++ bst-kth-smallest.cpp
@@ -33,9 +33,42 @@
     }
 
 //Optimization: Add an lcount (number of left children count) for each node, during insertion. kthsmallest becomes very simple then.
+struct Node {
+      int val;
+      int lcount;
+      Node *left;
+      Node *right;
+      Node(int x) : val(x), left(NULL), right(NULL) {}
+  };
 
-    
+Node* insert(Node* root, int key) {
+  Node * newN = newNode(key);
+  if(!root) return newN;
+  Node* cur = root;
+  Node * parent = NULL;
+  
+  while(cur) {
+    parent = cur;
+    if(key < cur->val) {cur->lcount++;cur = cur->left;}
+    else cur = cur->right;
+  }
+  newN->parent = parent;
+  if(!parent) root = newN;
+  if(key < parent->val) parent->left = newN;
+  else parent->right = newN;
+  return root;
+}
+
+int kthSmallest(Node* root, int k) {
+ Node *cur - root;
+ while(cur){
+  if(k == cur->lcount + 1) return cur->val;
+  else if (k < cur->lcount) cur = cur->left;
+  else {k = k-cur->lcount;cur = cur->right;}
+ }
+ return -1;
+}
     /* Tested.
-     * Time complexity: O(n) where n is the number of nodes
+     * Time complexity: O(n) where n is the number of nodes for approach 1. O(log n) for approach 2
      * Space complexity: O(n)
      */
