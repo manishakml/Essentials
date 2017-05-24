6aadf0eb38b8f9fac2c6ae8d62cd7c1d3deb25e2
unyieldingGlacier <manishakml@gmail.com>
2017-05-24 16:20:16 -0700

    Create bt-pretty-print.cpp

---
+++ bt-pretty-print.cpp
@@ -0,0 +1,105 @@
+/* Print a binary tree. Need not print the edges, but print spaces well.
+ * for example, print Trees like:
+ *
+ *        1          
+ *       / \       
+ *      2   3        
+ *     /
+ *    4
+ *   / \
+ *  5   6
+ *
+ * into these:
+ *
+ *      1          
+ *     2 3       
+ *   4 
+ *  5 6
+ */
+ 
+ #include<iostream>
+#include<unordered_map>
+#include<stack>
+#include<queue>
+using namespace std;
+
+typedef struct Node {
+        int val;
+        struct Node * left;
+        struct Node* right;
+}Node;
+
+class Solution {
+unordered_map<Node*, int> m;
+
+public:
+Node* newNode(int k) {
+        Node* newN = (Node*)malloc(sizeof(Node));
+        newN->val = k;
+        newN->left = NULL;
+        newN->right = NULL;
+        return newN;
+}
+void inorder(Node *root) {
+        stack<Node *> s;
+        Node *cur = root;
+        while(cur) {
+                s.push(cur);
+                cur = cur->left;
+        }
+        int k = 0;
+        while(!s.empty()) {
+                Node* t = s.top();
+                s.pop();
+                m[t] = k++;
+                if(t->right) {
+                        Node *cur = t->right;
+                        while(cur){
+                                s.push(cur);
+                                cur = cur->left;
+                        }
+                }
+        }void print(Node* root) {
+        inorder(root);
+        queue<Node*> q;
+        if(!root) return;
+        q.push(root);
+        int prev_sp = 0;
+        while(!q.empty()){
+                int s = q.size();
+                for(int i = 0; i < s; i++) {
+                        Node* t = q.front();
+                        q.pop();
+                        int sp = m[t]-prev_sp;
+                        while(sp--){
+                                cout << ' ';
+                                prev_sp++;
+                        }
+                        cout << t->val;
+                        prev_sp++;
+                        if(t->left) q.push(t->left);
+                        if(t->right) q.push(t->right);
+                }
+                cout << endl;
+                prev_sp = 0;
+        }
+}
+};
+
+int main() {
+        Solution obj;
+        Node* root = obj.newNode(1);
+        root->left = obj.newNode(2);
+        root->right = obj.newNode(3);
+        root->left->left = obj.newNode(4);
+        root->left->left->left = obj.newNode(5);
+        root->left->left->right = obj.newNode(6);
+        obj.print(root);
+        return 1;
+}
+
+/* Not tested thoroughly.
+ * Time complexity: O(n) where n is the number of nodes.
+ * Space complexity: O(n)
+ */
+
