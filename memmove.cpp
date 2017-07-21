370b135e67d0ca0d84aae914aeee448b6d528855
unyieldingGlacier <manishakml@gmail.com>
2017-07-21 15:20:18 -0700

    Create memmove.cpp

---
+++ memmove.cpp
@@ -0,0 +1,22 @@
+/* Implement memmove */
+
+void *memmove(void *dst, void *src, size_t n) {
+  char *p = dst;
+  const char *q = src;
+  if(p > q){    // copy backwards
+    while(n--) {
+      *p-- = *q--;
+    }
+  } else {
+    while(n--){
+      *p++ = *q++;
+    }
+  }
+  return dst;
+}
+
+/* Not tested.
+ * Note: memmove can have overlapping src.dst - hence the different code paths.
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
