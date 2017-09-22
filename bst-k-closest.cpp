0f284a36398fb3767a3776cd6dbd20f0a7dbdf28
unyieldingGlacier <manishakml@gmail.com>
2017-09-22 13:13:27 -0700

    Create bst-k-closest.cpp

---
+++ bst-k-closest.cpp
@@ -0,0 +1,37 @@
+/* LC 272
+ * Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
+ * Note:
+ * Given target value is a floating point.
+ * You may assume k is always valid, that is: k ≤ total nodes.
+ * You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
+ * Follow up:
+ * Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
+ */
+ 
+ void inorder(TreeNode* root, double target, queue<int>& q, int k) {
+        if(!root) return;
+        inorder(root->left, target, q, k);
+        if(q.size() < k) q.push(root->val);
+        else if (q.size() == k) {
+            if(abs(root->val-target) < abs(q.front()-target)){
+                q.pop();
+                q.push(root->val);
+            }
+        }        
+        inorder(root->right, target, q, k);
+    }
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        vector<int> res;
+        queue<int> q;
+        inorder(root,target,q,k);
+        while(!q.empty()) {
+            res.push_back(q.front());
+            q.pop();
+        }
+        return res;
+    }
+    
+    /* Tested.
+     * Time complexity: O(n)
+     * Space complexity: O(k) for queue or O(log n) for call stack
+     */
