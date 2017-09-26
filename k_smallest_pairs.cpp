172c68dc83e65ba8b7fce58b610aefa33ab266c7
unyieldingGlacier <manishakml@gmail.com>
2017-09-26 15:02:30 -0700

    Create k_smallest_pairs.cpp

---
+++ k_smallest_pairs.cpp
@@ -0,0 +1,59 @@
+/* Similar to LC 373
+ * Given 2 arrays and k, find the first k pairs with the smallest product. The pair should have first element from array1 and second from array2 */
+ */
+ #include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+vector<pair<int,int> >k_smallest(vector<int> &A, vector<int>& B, int k) {
+    vector<pair<int,int> > res;
+    if(A.size() == 0 || B.size() == 0 || k == 0) return res;
+    auto cmp = [&A,&B](pair<int,int> a, pair<int,int> b){return A[a.first]*B[a.second] > A[b.first]*B[b.second];};
+    priority_queue<pair<int,int>, vector<pair<int,int> >, decltype(cmp)> min_heap(cmp);
+    for(int i = 0; i < min(k,(int)A.size()); i++) {
+        min_heap.push(make_pair(i,0));
+    }
+    while(res.size() < k && !min_heap.empty()){
+        pair<int,int> p = min_heap.top();
+        res.push_back(p);
+        min_heap.pop();
+        if(p.second < B.size()-1) {
+            min_heap.push(make_pair(p.first,p.second+1));
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<pair<int,int> >res;
+    int n,m,k;
+    cout << "\nEnter n:";
+    cin >> n;
+    cout << "\nEnter m:";
+    cin >> m;
+    vector<int> A(n);
+    vector<int> B(m);
+    cout << "\nEnter A of size n:\n";
+    for(int i = 0; i<n; i++) {
+        cin >> A[i];
+    }
+    cout << "\nEnter B of size m:\n";
+    for(int i = 0; i<m; i++) {
+        cin >> B[i];
+    }
+    cout << "\nEnter k:";
+    cin >> k;
+    res = k_smallest(A,B,k);
+    for(int i = 0; i < res.size(); i++) {
+        cout << endl << res[i].first << " " << res[i].second;
+    }
+    return 0;
+}
+
+/* Not tested thoroughly.
+ * Time complexity: O(nm)
+ * Space complexity: O(k)
+ */
