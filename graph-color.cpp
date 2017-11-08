04ed5bf0e6d0c404b14803a8f252e3c477ba4a67
unyieldingGlacier <manishakml@gmail.com>
2017-11-08 11:07:22 -0800

    Create graph-color.cpp

---
+++ graph-color.cpp
@@ -0,0 +1,43 @@
+/* Given a graph with a start node, can it be 2-colored? 2-colored means being able to color the nodes with one of the 2 colors and not having adjacent same colors. */
+
+// bfs - check for children being uncolored or being colored differently. If they are colored the same, there is a uncolorable cycle - return false
+#include<iostream>
+using namespace std;
+// already implemented
+vector<Node *> getNeighbors();
+int getColor();
+void setColor(int c);
+
+bool is2Colorable(Node *start) {
+    if(!start) return false;
+    queue<Node *> q;
+    q.push(start);
+
+    while(!q.empty()) {
+        Node *t = q.front();
+        q.pop();
+        //for start node
+        if(t.getColor() == 0) {
+            t.setColor(1);
+        }
+        //current color
+        int color = t.getColor();
+        vector<Node *> children = t.getChildren();
+        for(Node *c : children){
+            //if child has already been seen and is of the same color
+            if(c.getColor() == color) {
+                return false;
+            } else if (c.getColor() == 0) { //if child is uncolored
+                color == 1? c.setColor(2) : c.setColor(1);
+                q.push(c);
+            }
+            //if child is already seen and is of different color, we neglect it; don't add it to queue again (results in infinite loop)
+        }
+    }
+    return true;
+}
+
+/* Not tested.
+ * Time complexity: O(2*e) where e is the number of edges
+ * Space complexity: O(n) where n is the number of vertices
+ */
