804d5750126ccb1f74a01d1ac5df0e62b5abe079
unyieldingGlacier <manishakml@gmail.com>
2017-11-16 15:09:45 -0800

    Update expression-add-operators.cpp

--- expression-add-operators.cpp
+++ expression-add-operators.cpp
@@ -71,8 +71,47 @@ void helper(vector<string> &R, string result, long long prev_op, long long total
         helper(R,"",0,0,target,num,0);
         return R;
     }
-    
-    /* Tested.
+    //To add division as well: watch out for /0
+    #include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void helper(vector<string> &R, string result, long long prev_op, long long total, int target, string num,int pos){
+        if(pos == num.length()) {
+            if(target == total) {
+                R.push_back(result);
+            }
+            return;
+        }
+
+        for(int i = pos; i < num.length(); i++) {
+            string sub = num.substr(pos,i-pos+1);
+            if(i > pos && num[pos] == '0') {
+                return;
+            }
+            long long val = stoll(sub);
+            if(pos > 0) {
+                helper(R,result+"+"+sub,val,total+val,target,num,i+1);
+                helper(R,result+'-'+sub,-val,total-val,target,num,i+1);
+                helper(R,result+'*'+sub,prev_op*val,(total-prev_op)+(prev_op*val),target,num,i+1);
+                if(val){
+                        helper(R,result+'/'+sub,prev_op/val,(total-prev_op)+(prev_op/val),target,num,i+1);
+                }
+            } else {
+                helper(R,sub,val,val,target,num,i+1);
+            }
+        }
+
+    }
+
+    vector<string> addOperators(string num, int target) {
+        vector<string> R;
+        helper(R,"",0,0,target,num,0);
+        return R;
+    }
+
+    /* Tested. Division not tested toroughly.
      * Time complexity: O(n^n) - exponential. Consider a recursion tree. It starts with n n chars and goes until 0 chars. So, ht is n. At each level, it forks into 3 (+-*). Each of these 3 are subdivided into (n-1) branches. So, 3n + (3^2)(n-1)...
      * Space complexity: O(n) = height of the tree.
      */
