29337872bbf7edd6905600bfa3e58f225cce17d9
unyieldingGlacier <manishakml@gmail.com>
2017-11-17 14:57:00 -0800

    Create filter-iterator.cpp

---
+++ filter-iterator.cpp
@@ -0,0 +1,46 @@
+/* Implement a specialized iterator called filterIterator which iterates over elements that satisfy a condition given to us, called predicate. */
+//This is just a prototype. Assumptions: C++ has an iterator like Java, with next(), hasNext() and remove().
+
+Iterator it;   //assuming c++ has somehting similar
+bool apply();   //predicate/condition given to us
+
+template<typename T>
+class filterIterator extends Iterator {
+    T *res = nullptr;
+    public:
+        filterIterator(){
+            while(it.hasNext()){
+                T tmp = it.next();
+                if(tmp.apply()) {
+                    *res = tmp;
+                    break;
+                }// apply the predicate/condition
+            }
+        }
+
+        bool hasNext(){
+            return (res != nullptr);
+        }
+
+        T next(){
+            T result = *res;
+            res = nullptr;
+
+            while(it.hasNext()){
+                T tmp = it.next();
+                if(tmp.apply()){
+                    *res = tmp;
+                    break;
+                }
+            }
+            return result;
+        }
+        //remove() cannot be safely implemented because next() returns an elemtn but progresses towards the next e
+ligible element. So, calling remove() using underlying it.remove() will cause some other further element to be rem
+oved and not the intended one.
+};
+
+/* Not tested.
+ * Time complexity: hasNext() is O(1), next() is O(n) ssuming the container has n elements.
+ * Space complexity: O(1).
+ */
