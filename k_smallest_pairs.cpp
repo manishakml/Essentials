e47dccd0b16bdd3083c6d704aa10c927ddbda0f7
unyieldingGlacier <manishakml@gmail.com>
2017-11-21 17:25:13 -0800

    Update k_smallest_pairs.cpp

--- k_smallest_pairs.cpp
+++ k_smallest_pairs.cpp
@@ -11,13 +11,16 @@ using namespace std;
 vector<pair<int,int> >k_smallest(vector<int> &A, vector<int>& B, int k) {
     vector<pair<int,int> > res;
     if(A.size() == 0 || B.size() == 0 || k == 0) return res;
+ //sort based on product. Use '>' since min_heap uses greater()
     auto cmp = [&A,&B](pair<int,int> a, pair<int,int> b){return A[a.first]*B[a.second] > A[b.first]*B[b.second];};
     priority_queue<pair<int,int>, vector<pair<int,int> >, decltype(cmp)> min_heap(cmp);
+ //Pair all elements of A with first element of B until heap is k size or all A elements are exhausted.
     for(int i = 0; i < min(k,(int)A.size()); i++) {
         min_heap.push(make_pair(i,0));
     }
     while(res.size() < k && !min_heap.empty()){
         pair<int,int> p = min_heap.top();
+     //A[0],B[0] is guaranteed to be part of the result. For future comparisons, we push more elements in order
         res.push_back(p);
         min_heap.pop();
         if(p.second < B.size()-1) {
