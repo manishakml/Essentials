cec36547961bb7ccbb0e978a71a7e00030dce8e7
unyieldingGlacier <manishakml@gmail.com>
2017-11-21 18:40:19 -0800

    Update pizza.cpp

--- pizza.cpp
+++ pizza.cpp
@@ -18,3 +18,56 @@
   * Time complexity: O(1)
   * Space complexity: O(1)
   */
+
+/* If we are given n, write an algorithm to produce n lines based on the above conditions. */
+class line {
+ public:
+ double m;
+ double c;
+ line(int m, int c){this.m = m; this.c = c;}
+};
+
+vector<line> draw(int n){
+ vector<line> res;
+ double m = 0, c = 0;
+ for(int i = 0; i < n; i++){
+  res[i].push_back(new line(m,c));
+  //increase m and c by 1 (or any arbitrary amt) progressively to get unique lines that intersect all previous lines.
+  m += 1;
+  c += 1;
+ }
+ return res;
+}
+
+/* Not tested.
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ */
+
+/* If we are given n lines, write an algorithm to generate the (n+1)th line based on above conditions. */
+line generate(vector<line> &A) {
+ set<pair<double,double>> invalid;  // keeps track of non-unique intersections.
+ int max_slope = INT_MIN;
+ for(int i = 0; i < A.size(); i++) {
+  max_slope = max(max_slope, A[i].m);
+ }
+ double slope = max_slope + 1;   // we could find min_slope and use min_slope-1 too. All we need is a unique slope.
+ 
+ for(int i = 0; i < A.size()-1; i++){
+  for(int j = i+1; j < A.size(); j++) {
+   pair<double,double> intersect;
+   intersect.first = (A[j].c-A[i].c)/(A[i].m-A[j].m);  //formula for intersection. y = m1x+c1; y = m2x+c2. Calc. x in terms of m and c.
+   intersect.second  = A[i].m*intersect.first + A[i].c //y = mx+c; x has been calc above.
+   s.insert(intersect); // don't use this intersection for the new line
+  }
+ }
+ double intercept = *s.rbegin() + 1; //Use any increment here. We can even use min_element -1. Set orders in increasing order.
+ return new line(slope,intercept);
+}
+
+/* Not tested.
+ * Time complexity: O(n^2)
+ * Space complexity: O(n) for the intersections.
+ */
+
+
