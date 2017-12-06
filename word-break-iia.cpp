f0b5abd06d301bf9fb6693ab1659c7913146427e
unyieldingGlacier <manishakml@gmail.com>
2017-12-06 09:25:37 -0800

    Update word-break-iia.cpp

--- word-break-iia.cpp
+++ word-break-iia.cpp
@@ -7,19 +7,25 @@
 using namespace std;
 
 string helper(string s, unordered_set<string> &d, unordered_map<string,string> &m){
-    //if(m.find(s) != m.end()) return m[s];
+    //if previously computed in prev. recursive calls, use that result.
+    if(m.find(s) != m.end()) return m[s];
+    //if it's a dictionary word, use that result. We return because we need only one combination.
     if(d.find(s) != d.end()) return s;
     string res = "";
     for(int sz = 1; sz < s.length(); sz++){
+        //substring of incremental sizes which becomes the prefix
         string sub = s.substr(0,sz);
         if(d.find(sub) != d.end()){
+            //suffix
             string str = helper(s.substr(sz),d,m);
             res = sub + " " + str;
-            //m[s] = res;
+            //store it for future use
+            m[s] = res;
             return res;   //One valid result found, return.
         }
     }
-    //m[s] = res;
+    //store res as ""
+    m[s] = res;
     return res;
 }
 
@@ -39,6 +45,6 @@ int main(){
 }
 
 /* Not tested thoroughly.
- * Time complexity: O(n!) without memoization
+ * Time complexity: O(n!). Memoization avoid re-solving few of the same subproblems. Consider "xyzxyabc" and {xyz,xy,z}. First recursive call (sz = 2): xy z xy "" So, map has {{abc,""},{xyabc,xy},{zxyabc,z xy},{xyzxyabc,xy z xy}}. 2nd recursive call (sz = 3): xyz helper(xyabc) = xyz (from map)xy.
  * Space complexity: O(n)
  */
