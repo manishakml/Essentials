0bfeb0cb1ac78d6a2d148b704235f8e602932c0d
unyieldingGlacier <manishakml@gmail.com>
2017-12-20 15:01:29 -0800

    Update longest-palindromic-subsequence.cpp

--- longest-palindromic-subsequence.cpp
+++ longest-palindromic-subsequence.cpp
@@ -21,7 +21,7 @@
  int longestPalindromeSubseq(string s) {
         int n = s.length();
         int dp[n][n];
-        memset(dp,1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         
         for(int i = 0; i < n; i++) {
             dp[i][i] = 1;
