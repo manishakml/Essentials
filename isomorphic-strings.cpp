4c632c7586dc1d3f0f7e4a0724a09f1db613f687
unyieldingGlacier <manishakml@gmail.com>
2018-01-01 14:59:58 -0800

    Update isomorphic-strings.cpp

--- isomorphic-strings.cpp
+++ isomorphic-strings.cpp
@@ -16,7 +16,7 @@
  bool isomorphic(string s, string t) {
   int m1[256] = {0}, m2[256] = {0}, n = s.length();
   for(int i = 0; i < n; i++) {
-    if(m1[s[i] != m2[t[i]]) {
+    if(m1[s[i]] != m2[t[i]]) {
       return false;
     }
     m1[s[i]] = m2[t[i]] = i+1;
