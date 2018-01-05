ac9591bc9df82265f45463069f2429f2f10fb03e
unyieldingGlacier <manishakml@gmail.com>
2018-01-04 23:06:50 -0800

    Create subset-partition.cpp

---
+++ subset-partition.cpp
@@ -0,0 +1,18 @@
+/* Given an array and a target, find the number of subsets that sum up to the target. */
+
+int helper(vector<int> &A, int k, int idx) {
+  if(idx >= A.size()){
+    return 0;
+  }
+  return (A[idx] == k? 1 : 0) + helper(A,k,idx+1) + helper(A,k-A[idx],idx+1); //if elem equals sum, add 1; count n_subsets including elem and n_subsets excluding element and add them.
+}
+
+int subset_count(vector<int> &A, int k){
+  return helper(A,k,0);
+}
+
+/* Not tested.
+ * Note: (1,2,3,4) and target 4: 
+ * Time complexity: O(n!)
+ * Space complexity: O(n)
+ */
