68d04a82bf31a1e870dc59e6310cadfe0d2da06f
unyieldingGlacier <manishakml@gmail.com>
2018-01-16 01:11:25 -0800

    Update lookup-sorted-range.cpp

--- lookup-sorted-range.cpp
+++ lookup-sorted-range.cpp
@@ -32,6 +32,40 @@ int main(){
     return 1;
 }
 
+//approach 2 - handles invalid characters
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int lookup(vector<char> &A, char k){
+        if(A.size() == 0 || k < 'a' || k > 'z'){
+                return -1;
+        }
+        if(k < A[0]) return 0;
+        if(k >= A[A.size()-1]) return 0;
+
+        int l = 0, r = A.size()-1;
+
+        while(l <= r) {
+                int m = l + (r-l)/2;
+                if(A[m] > k && (m == 0 || A[m-1] <= k)){
+                        return m;
+                } else if(A[m] > k) {
+                        r = m-1;
+                } else {
+                        l = m+1;
+                }
+        }
+        return -1;
+}
+
+int main(){
+        vector<char> A = {'c','f','j','p','v'};
+        char k = 'A';
+        cout << lookup(A,k);
+        return 1;
+}
+
 /* Not tested thoroughly.
  * Time complexity: O(log n) where n is the number of elements in the list.
  * Space complexity: O(1)
