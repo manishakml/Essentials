a1e4c1939b63922144087e9e82e24dc1c5cf2ae3
unyieldingGlacier <manishakml@gmail.com>
2018-01-15 17:21:12 -0800

    Update valid-parenthesis.cpp

--- valid-parenthesis.cpp
+++ valid-parenthesis.cpp
@@ -23,6 +23,35 @@ bool isValid(string s) {
         return st.empty();
 }
 
+//approach 2
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            } else if(s[i] == ')') {
+                if(st.empty() || st.top() != '('){
+                    return false;
+                }
+                st.pop();
+            } else if(s[i] == ']') {
+                if(st.empty() || st.top() != '['){
+                    return false;
+                }
+                st.pop();
+            } else if(s[i] == '}') {
+                if(st.empty() || st.top() != '{'){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
 /* Tested.
  * Time complexity: O(n) where n is the length of the string.
  * Space complexity: O(n)
