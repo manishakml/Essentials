fa3a706bbcd3b2f8ac7c022f9b7c5606aa71ecd5
unyieldingGlacier <manishakml@gmail.com>
2018-01-15 16:58:15 -0800

    Create to-binary.cpp

---
+++ to-binary.cpp
@@ -0,0 +1,25 @@
+/* Convert a given number to binary */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string toBinary(int n){
+        string res = "";
+        while(n){
+                res += to_string(n%2);
+                n /= 2;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+}
+
+int main(){
+        cout << toBinary(38);
+        return 1;
+}
+
+/* Not tested thoroughly.
+ * Time complexity: O(log n)
+ * Space complexity: O(1)
+ */
