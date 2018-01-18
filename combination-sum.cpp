cec7e828925f3801db09795676fc3ff1ae2a3d67
unyieldingGlacier <manishakml@gmail.com>
2018-01-17 17:05:59 -0800

    Update combination-sum.cpp

--- combination-sum.cpp
+++ combination-sum.cpp
@@ -11,7 +11,7 @@
  * [2, 2, 3]
  * ]
  */
-
+//this approach sorts but there is not much use.
 void helper(vector<int>& candidates, int target, vector<vector<int>> &res, vector<int> &ans, int pos) {
         if(target < 0) {
             return;
@@ -33,7 +33,7 @@ void helper(vector<int>& candidates, int target, vector<vector<int>> &res, vecto
         return res;
     }
 
-//sorting does not give us an advantage
+//sorting does not give us an advantage until we make use of it
 class Solution {
 public:
     void helper(vector<int> &candidates, int sum, vector<vector<int>> &res, vector<int> &ans, int remain, int start){
@@ -55,6 +55,32 @@ public:
         helper(candidates,target,res,ans,target,0);
         return res;
     }
+        
+        //making use of sorting to prune the result. Suppose[2,3,4,5,6,7,8,9] and target is 7. After processing 7, we dont need to process 8 and 9.
+        class Solution {
+public:
+    void helper(vector<int> &candidates, int sum, vector<vector<int>> &res, vector<int> &ans, int remain, int start){
+        if(remain < 0) {
+            return;
+        } else if(remain == 0){
+            res.push_back(ans);
+        } else {
+            for(int i = start; i < candidates.size(); i++){
+                if(candidates[i] > sum) return; //or break;
+                ans.push_back(candidates[i]);
+                helper(candidates,sum,res,ans,remain-candidates[i],i);
+                ans.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> ans;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates,target,res,ans,target,0);
+        return res;
+    }
+};
 };
     
     /* Tested.
