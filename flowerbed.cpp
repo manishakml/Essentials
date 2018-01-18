444bce234671fb78f52caec6a1a081052d7bddd8
unyieldingGlacier <manishakml@gmail.com>
2018-01-18 11:57:32 -0800

    Update flowerbed.cpp

--- flowerbed.cpp
+++ flowerbed.cpp
@@ -4,25 +4,31 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        //if there are no new flowers to be planted
         if(n <= 0) {
                 return true;
         }
         int N = flowerbed.size();
+        //if the number of slots is lesser than the given flowers. Note this should not be <=. Counter case: If n = 1 and N = 1 and A[0] = 0, it is true.
         if(N < n) {
                 return false;
         }
 
         for(int i = 0; i < N; i++){
+            //if free slot and its neighbor(s) are false, decrement count. If count is 0, break. Otherwise loop will keep running for negative vals of n and result in wrong answer.
                 if(flowerbed[i] == 0 && ((i == 0 || flowerbed[i-1] == false) && (i == N-1 || flowerbed[i+1] == false))){
                         n--;
                         if(n == 0){
                             break;
                         }
+                        //once planted, its negihbor is not free, so can move by 2. once here and once in the loop
                         i++;
                 } else if(flowerbed[i] == 1){
+                    //it's not free. next slot is not useful, so skip it.
                         i++;
                 }
         }
+        //only if the given flowers are exhausted, return true.
         return n == 0;
     }
 };
