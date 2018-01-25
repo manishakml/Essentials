88f94511c2cc07e297c2109c9ede3c8632d4b450
unyieldingGlacier <manishakml@gmail.com>
2018-01-25 13:50:54 -0800

    Update filter-iterator.cpp

--- filter-iterator.cpp
+++ filter-iterator.cpp
@@ -38,6 +38,39 @@ class filterIterator extends Iterator {
         //remove() cannot be safely implemented because next() returns an elemtn but progresses towards the next eligible element. So, calling remove() using underlying it.remove() will cause some other further element to be removed and not the intended one.
 };
 
+template <class Item>
+class fiterIterator : cursor<Item>{
+    cursor<Item> source_;
+    bool (&selector_)(Item);
+    Item res;
+public:
+    filterIterator(cursor<Item> &src, bool (&selector)(Item)){
+        source_ = src;
+        selector_ = selector;
+        res = nullptr;
+    }
+    ~filterIterator(){}
+    bool done(){
+        while(!source_.done()){
+            Item cur = source_.read();
+            if(selector_(cur)){
+               res = cur;
+               return false;
+            }
+         }
+         return true;
+    }
+    
+    Item read(){
+        if(done()){
+            throw logic_error("Calling read without checking done().");
+        }
+        Item result = res;
+        res = nullptr;
+        return result;
+    }
+};
+
 /* Not tested.
  * Time complexity: hasNext() is O(1), next() is O(n) ssuming the container has n elements.
  * Space complexity: O(1).
